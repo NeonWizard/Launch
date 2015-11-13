74e7cedc26035dbbab4f80dc5bef9a2126cb45e0
Anthony <pategeorge12@gmail.com>
2015-11-13 10:26:33 -0700

    again

--- rocket.py
+++ rocket.py
@@ -5,7 +5,7 @@ class Rocket:
 	def __init__(self, image):
 		self.image = pygame.image.load(image)
 		self.screenPosX = config.SCREEN_WIDTH / 2 - self.image.get_width() / 2
-		self.screenPosY =  config.SCREEN_HEIGHT - 200
+		self.screenPosY =  config.SCREEN_HEIGHT - 400
 
 	def draw(self, surface):
 		surface.blit(self.image, (self.screenPosX, self.screenPosY))
