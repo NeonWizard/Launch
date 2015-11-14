ce261d61cf1e899031f34d7447db5ad5f108b733
Average Wizard <thelordofgiggles@gmail.com>
2015-11-13 23:12:25 -0700

    Touched up terrain gen

--- terraingen.py
+++ terraingen.py
@@ -8,7 +8,18 @@ def generateHills(surfSize, hills):
 
 	surf.fill(TRANSPARENCY)
 
+	toBlit = []
+
 	for i in range(hills):
-		pygame.draw.circle(surf, [0, 255, 0], (surfSize[0]/hills*i+random.randrange(-50, 50), surf.get_height()), random.randrange(surfSize[0]/hills, surfSize[0]/hills*1.5))
+		radius = random.randrange(surfSize[0]/hills, surfSize[0]/hills*1.5)
+		pos = (surfSize[0]/(hills-2)*(i-1)+random.randrange(-50, 0)*2, surf.get_height()+radius/2)
+		toBlit.append(((0, 255, 0), pos, radius))
+
+	random.shuffle(toBlit)
+
+	for i in toBlit:
+		pygame.draw.circle(surf, i[0], i[1], i[2])
+		pygame.draw.circle(surf, (0, 0, 0), i[1], i[2]+1, 1)
+
 
 	return surf
